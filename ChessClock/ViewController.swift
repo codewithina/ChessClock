b30e6cba0344bb19f74fc53d2abf8201c01400a4
Ina Burström <inaburstrom@hotmail.com>
2024-03-12 08:42:19 +0100

    Added label for  p1 and p2 timer.

--- ChessClock/ViewController.swift
+++ ChessClock/ViewController.swift
@@ -12,10 +12,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var p1Timer: UIView!
     @IBOutlet weak var p2Timer: UIView!
     
+    @IBOutlet weak var p1TimeLabel: UILabel!
+    @IBOutlet weak var p2TimeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        p1Timer.backgroundColor = UIColor.lightGray
+        p1Timer.backgroundColor = UIColor.blue
+        p2Timer.backgroundColor = UIColor.blue
+        print("Hello")
     }
     
 }
